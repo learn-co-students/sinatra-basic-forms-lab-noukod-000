6c299eb9abdc2d5fb100428b9a800d0b3101fde4
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-03 19:54:31 +0000

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
-class Puppy 
-attr_accerssor :name,:breed,:age
+class Puppy
+attr_accessor :name,:breed,:age
 
 	def initialize(name,breed,age)
  		@name = name
  		@breed = breed
  		@age =  age
-	end 
+	end
 
-end 
+end
